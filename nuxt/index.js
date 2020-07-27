f99d313c61e360ea5e6a5f19ce4d198e1fd4875a
Eduardo San Martin Morote <posva13@gmail.com>
2020-07-27 12:26:58 +0200

    fix: ignore some headers when proxying

--- nuxt/index.js
+++ nuxt/index.js
@@ -1,8 +1,28 @@
 import path from 'path'
 
-export default function NuxtMandeModule() {
+const DEFAULT_OPTIONS = {
+  proxyHeadersIgnore: [
+    'accept',
+    'host',
+    'cf-ray',
+    'cf-connecting-ip',
+    'content-length',
+    'content-md5',
+    'content-type',
+  ],
+}
+
+/** @type {import('@nuxt/types').Module} */
+const MandeModule = function NuxtMandeModule(localOptions) {
+  // TODO: merge arrays properly. There is probably a package to handle this
+  const options = {
+    ...DEFAULT_OPTIONS,
+    ...localOptions,
+  }
+
   this.addPlugin({
     src: path.resolve(__dirname, 'plugin.js'),
     fileName: 'mande.js',
+    options,
   })
 }
--- nuxt/plugin.js
+++ nuxt/plugin.js
@@ -1,10 +1,23 @@
+
 /** @type {import('@nuxt/types').Plugin} */
 export default (ctx, inject) => {
   function mande(wrappedFn, ...args) {
     return wrappedFn(
       /** @type {import('../src').MandeInstance} */
       (api) => {
-        api.options.headers = { ...api.options.headers, ...ctx.req.headers }
+        const reqHeaders = { ...ctx.req.headers }
+
+        // @ts-ignore
+        for (let header of <%= serialize(options.proxyHeadersIgnore) %>) {
+          delete reqHeaders[header]
+        }
+
+        api.options.headers = { ...api.options.headers, ...reqHeaders }
+
+        if (process.server) {
+          // Don't accept brotli encoding because Node can't parse it
+          api.options.headers['accept-encoding'] = 'gzip, deflate'
+        }
       },
       ...args
     )
