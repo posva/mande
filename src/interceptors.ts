32cf0afdc2b68ba842906618c3e342e5a29ff92d
Eduardo San Martin Morote <posva13@gmail.com>
2023-06-02 11:26:25 +0200

    wip: interceptors

---
+++ src/interceptors.ts
@@ -0,0 +1,46 @@
+import { MandeError, MandeInstance, _OptionsMerged } from './mande'
+
+/**
+ * Mande instance with interceptors.
+ */
+export interface MandeInstanceWithInterceptors extends MandeInstance {
+  onRequest(_context: {
+    url: string
+    options: _OptionsMerged
+  }): _MaybePromise<void>
+
+  onResponse(_context: {
+    url: string
+    options: _OptionsMerged
+    response: Response
+  }): unknown
+
+  onError<Error>(_context: {
+    url: string
+    options: _OptionsMerged
+    error: MandeError<Error>
+  }): _MaybePromise<MandeError | null | false | undefined | void>
+}
+
+/**
+ * Internal type to define a function that may return a Promise.
+ * @internal
+ */
+type _MaybePromise<T> = T | PromiseLike<T>
+
+/**
+ * NOTES: We currently pass a string and RequestInit options to fetch requests rather than doing new Request(url, options). Not sure if it's worth refactoring because Request is immutable (cannot change the url or body) and silently fails when changing properties
+ */
+
+
+export function withInterceptors(mande: MandeInstance): MandeInstanceWithInterceptors {
+  const mandeWithInterceptors = mande as MandeInstanceWithInterceptors
+
+  mandeWithInterceptors.onRequest = () => { }
+  mandeWithInterceptors.onResponse = () => { }
+  mandeWithInterceptors.onError = () => { }
+
+  // TODO: override
+
+  return mandeWithInterceptors
+}
